c4bbbec769aef216c2f170002703b3b6bf244952
harishgoud1729 <51473162+harishgoud1729@users.noreply.github.com>
2022-07-08 18:23:42 +0530

    Add files via upload

---
+++ social_distance_detector.py
@@ -0,0 +1,125 @@
+from Batch_25 import social_distancing_config as config
+from Batch_25.detection import detect_people
+from scipy.spatial import distance as dist
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+	help="path to (optional) input video file")
+ap.add_argument("-o", "--output", type=str, default="",
+	help="path to (optional) output video file")
+ap.add_argument("-d", "--display", type=int, default=1,
+	help="whether or not output frame should be displayed")
+args = vars(ap.parse_args())
+labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# check if we are going to use GPU
+if config.USE_GPU:
+	# set CUDA as the preferable backend and target
+	print("[INFO] setting preferable backend and target to CUDA...")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream and pointer to output video file
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture(r"C:\Users\haris\Downloads\WhatsApp Video 2022-07-07 at 10.53.31 AM.mp4")
+#args["input"] if args["input"] else 0
+#"C:\Users\haris\OneDrive\Desktop\Social-distance-detection-master\pedestrians.mp4"
+#r"C:\Users\haris\OneDrive\Desktop\sample\depositphotos_27545223-stock-video-comes-in-building-in-united.mp4"
+writer = None
+
+# loop over the frames from the video stream
+while True:
+	# read the next frame from the file
+	(grabbed, frame) = vs.read()
+
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+
+	# resize the frame and then detect people (and only people) in it
+	frame = imutils.resize(frame, width=700)
+	results = detect_people(frame, net, ln,
+		personIdx=LABELS.index("person"))
+
+	# initialize the set of indexes that violate the minimum social
+	# distance
+	violate = set()
+
+	if len(results) >= 2:
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
+
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				# check to see if the distance between any two
+				# centroid pairs is less than the configured number
+				# of pixels
+				if D[i, j] < config.MIN_DISTANCE:
+					# update our violation set with the indexes of
+					# the centroid pairs
+					violate.add(i)
+					violate.add(j)
+
+	# loop over the results
+	for (i, (prob, bbox, centroid)) in enumerate(results):
+		# extract the bounding box and centroid coordinates, then
+		# initialize the color of the annotation
+		(startX, startY, endX, endY) = bbox
+		(cX, cY) = centroid
+		color = (0, 255, 0)
+
+		# if the index pair exists within the violation set, then
+		# update the color
+		if i in violate:
+			color = (0, 0, 255)
+
+		# draw (1) a bounding box around the person and (2) the
+		# centroid coordinates of the person,
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		cv2.circle(frame, (cX, cY), 5, color, 1)
+
+	# draw the total number of social distancing violations on the
+	# output frame
+	text = "Social Distancing Violations: {}".format(len(violate))
+	cv2.putText(frame, text, (10, frame.shape[0] - 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+
+	# check to see if the output frame should be displayed to our
+	# screen
+	if args["display"] > 0:
+		# show the output frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+	# if an output video file path has been supplied and the video
+	# writer has not been initialized, do so now
+	if args["output"] != "" and writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 25,
+			(frame.shape[1], frame.shape[0]), True)
+
+	# if the video writer is not None, write the frame to the output
+	# video file
+	if writer is not None:
+		writer.write(frame)
